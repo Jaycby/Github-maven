88dd2cde5ffa8c3f8b98b3110c2ce2581445701f
cby <cby@qq.com>
2022-12-02 16:55:23 +0800

    first commit

---
+++ src/main/java/com/Jaycby/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.Jaycby.git;
+
+/**
+ * @ClassName GitTest
+ * @Author cby
+ * @Date 2022/12/2 16:52
+ * @Version 1.0
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello github!!!");
+    }
+}
