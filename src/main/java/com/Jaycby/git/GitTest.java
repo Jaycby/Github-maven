abd245230ad12214b2d3d01205e81df8ed61e5c7
cby <cby@qq.com>
2022-12-02 17:05:40 +0800

    hot-fix first commit

--- src/main/java/com/Jaycby/git/GitTest.java
+++ src/main/java/com/Jaycby/git/GitTest.java
@@ -15,5 +15,7 @@ public class GitTest {
         System.out.println("hello github second!!!");
 
         System.out.println("hello github third!!!");
+
+        System.out.println("hot-fix first!!!");
     }
 }
