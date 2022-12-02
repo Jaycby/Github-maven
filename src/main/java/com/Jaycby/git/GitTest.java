d08aa3f41c55a7d334f6e5ab4363dfeadba3e605
cby <cby@qq.com>
2022-12-02 16:58:03 +0800

    second commit

--- src/main/java/com/Jaycby/git/GitTest.java
+++ src/main/java/com/Jaycby/git/GitTest.java
@@ -11,5 +11,7 @@ public class GitTest {
     public static void main(String[] args) {
 
         System.out.println("hello github!!!");
+
+        System.out.println("hello github second!!!");
     }
 }
