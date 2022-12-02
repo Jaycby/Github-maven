977df5e52d6f5630808c0289c6ccf7c64654b3e6
cby <cby@qq.com>
2022-12-02 17:37:25 +0800

    push first commit

--- src/main/java/com/Jaycby/git/GitTest.java
+++ src/main/java/com/Jaycby/git/GitTest.java
@@ -17,5 +17,7 @@ public class GitTest {
         System.out.println("hello github third!!!");
 
         System.out.println("hot-fix first!!!");
+
+        System.out.println("push first!!!");
     }
 }
